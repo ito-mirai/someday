2624dd654f00f8521b6c83ab14ff3cfae7ff23be
ito-mirai <tetudounohi@gmail.com>
2024-05-28 12:34:06 +0900

    モデルの設定

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,6 @@
 class Group < ApplicationRecord
+  belongs_to :user
+  has_many :tasks
+
+  validates :group_name, presence: true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+  belongs_to :group
+
+  validates :content, presence: true
+  validates :type_id, numericality: { other_than: 1 }
 end
