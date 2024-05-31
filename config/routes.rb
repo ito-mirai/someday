fb6dd6c1e52a3488a7b1994ec075948cd502295f
ito-mirai <tetudounohi@gmail.com>
2024-05-31 15:46:38 +0900

    taskの編集機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tasks#index'
   resources :groups, only: [:new, :create, :show, :update]
-  resources :tasks, only: [:index, :new, :create] do
+  resources :tasks, only: [:index, :new, :create, :show, :update] do
     resources :finishes, only: [:create, :destroy]
   end
 end
