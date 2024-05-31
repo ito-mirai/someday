9dd8e4ee53d1a31071151181f1eb2619716195d8
ito-mirai <tetudounohi@gmail.com>
2024-05-31 14:23:28 +0900

    チェックボックスを使ってfinishレコードの追加削除の実装

--- app/controllers/finishes_controller.rb
+++ app/controllers/finishes_controller.rb
@@ -1,8 +1,26 @@
 class FinishesController < ApplicationController
   def create
-    @finish = Finish.new
+    finish = Finish.create(finish_params)
+    if finish.save
+      render json: { id: finish.id }, status: :created
+    else
+      render json: { errors: finish.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    finish = Finish.find(params[:id])
+    if finish.destroy
+      head :no_content
+    else
+      render json: { errors: finish.errors.full_messages }, status: :unprocessable_entity
+    end
   end
+
+  private
+
+  def finish_params
+    params.require(:finish).permit(:task_id)
+  end
+
 end
