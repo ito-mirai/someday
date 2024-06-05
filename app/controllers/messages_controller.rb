024df8908cd6e04d41ebda376a91e7d274900666
ito-mirai <tetudounohi@gmail.com>
2024-06-05 12:57:40 +0900

    チャット機能の処理単位区分けとリファクタリング

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,30 +1,31 @@
 class MessagesController < ApplicationController
 
+  # 全てのmessageを取得する
+  before_action :messages_all, only: [:new, :create]
+
   def new
-    @messages = Message.all
     @message = Message.new
   end
 
   def create
-    @messages = Message.all
-    
-    hash = [{ role: "system", content: "あなたは優しいAIアシスタントです。優しい言葉でユーザーと対話してください" }]
-    @messages.each do |message|
-    
-      if message.speaker == 0
-        hash << {role: "user", content: message.message }
-      elsif message.speaker == 1
-        hash << {role: "system", content: message.message }
-      end
+    # ユーザーとAIの双方のメッセージが正常に保存された時のみ完了する
+    ActiveRecord::Base.transaction do
+      
+      # ユーザーのメッセージを保存
+      user_message = Message.new(message_params)
+      user_message.save!
 
+      # ChatGptServiceサービスでの処理
+      gpt = ChatGptService.new
+
+      new_message = message_params[:message]
+      response = gpt.chinese_room(@messages, new_message)
+
+      # AIのメッセージを保存
+      gpt_message = Message.new(message: response, speaker: 1, message_type: 0, user_id: current_user.id)
+      gpt_message.save!
     end
-
-    Message.create(message_params)
-    prompt = message_params[:message]
-    gpt = ChatGptService.new
-    hash << { role: "user", content: prompt }
-    gpt_message = gpt.chat(hash)
-    Message.create(message: gpt_message, speaker: 1, message_type: 0, user_id: current_user.id)
+    
     redirect_to new_message_path
   end
 
@@ -32,6 +33,10 @@ class MessagesController < ApplicationController
   end
 
   private
+
+  def messages_all
+    @messages = Message.all
+  end
 
   def message_params
     params.require(:message).permit(:message).merge(user_id: current_user.id, speaker: 0, message_type: 0)
--- app/services/chat_gpt_service.rb
+++ app/services/chat_gpt_service.rb
@@ -5,15 +5,47 @@ class ChatGptService
     @openai = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
   end
 
-  def chat(hash)
+  # AIと会話する
+  def chinese_room(messages, new_message)
+    pronpts = pronpt(messages, new_message)
+    chat(pronpts)
+  end
+
+  private
+
+  # プロンプトの生成
+  def pronpt(messages, new_message)
+
+    # AIの性格を規定
+    prompt = [{ role: "system", content: "あなたは優しいAIアシスタントです。優しい言葉でユーザーと対話してください" }]
+
+    # 会話履歴をプロンプトに含む
+    messages.each do |message|
+      # ユーザーのメッセージ
+      if message.speaker == 0
+        prompt << {role: "user", content: message.message }
+      # AIのメッセージ
+      elsif message.speaker == 1
+        prompt << {role: "system", content: message.message }
+      end
+    end
+
+    # 最新のメッセージをプロンプトに追加
+    prompt << { role: "user", content: new_message }
+
+  end
+
+  # プロンプトを使用したメッセージ生成
+  def chat(pronpt)
     response = @openai.chat(
       parameters: {
         model: "gpt-3.5-turbo", # Required. # 使用するGPT-3のエンジンを指定
-        messages: hash,
+        messages: pronpt,
         temperature: 0.7, # 応答のランダム性を指定
         max_tokens: 200,  # 応答の長さを指定
       },
       )
     response['choices'].first['message']['content']
   end
+
 end
