63382bff638b5d2a8b18c18708715aaffafbb7f0
ito-mirai <tetudounohi@gmail.com>
2024-06-06 20:48:38 +0900

    文脈から逸脱した時に再確認する機能を追加

--- app/services/chat_gpt_service.rb
+++ app/services/chat_gpt_service.rb
@@ -9,10 +9,18 @@ class ChatGptService
   def chinese_room(messages, user_message)
     @messages = messages
     @user_message = user_message
+
+    message_phase
     pronpt
   end
 
   private
+
+  # メッセージの進行具合を確認
+  def message_phase
+    select_message = @messages.select { |message| message.message_type <= 5 }
+    @message_phase = select_message.max_by(&:message_type)[:message_type]
+  end
 
   #-------------------------------------------------------------
 
@@ -23,12 +31,23 @@ class ChatGptService
     @prompt = [{ role: "system", content: "あなたはリマインダーアプリです。\nユーザーと対話して、ユーザーのタスクを引き出すことがあなたの役割です。\n\nあなたがユーザーに対して「何か困っていることはありますか？」と質問したので、ユーザーが回答します。あなたはその回答に基づいて、リマインダーに登録すべきタスクの概要を提案してください。\n\n　：返信例\n下記に、ユーザーの回答と望ましい返信例を列挙して提示します。なお提示する場合は、列挙するのではなく、一文だけ提示してください。\n\n1.「部屋が散らかっている」と、ユーザーが回答した場合\n部屋の整理をしましょう\n部屋の掃除をしましょう\n\n2.「リンスがない」と、ユーザーが回答した場合\nリンスを買いましょう\n日用品を買いに行きましょう\n\n3.「携帯料金の支払い用紙が来ている」と、ユーザーが回答した場合\n料金の支払いを済ませましょう" }]
 
     @gpt_array = []
-    if @messages.count == 1
+    if @message_phase == 0
       m_phase0
-      m_phase1
+      context_check_phase
+      if @check_result == "false"
+        @gpt_array.pop
+        message_again
+      else
+        m_phase1
+      end
   
-    else
+    elsif @message_phase == 2
       m_phase2
+      context_check_phase
+      if @check_result == "false"
+        @gpt_array.pop
+        message_again
+      end
 
     end
 
@@ -86,6 +105,26 @@ class ChatGptService
 
   end
 
+  # ----------文脈チェックフェーズ----------
+  def context_check_phase
+
+    # 文脈チェック用プロンプトの追加（チェック結果出力時に削除）
+    @prompt << {role: "system", content: "あなたは会話の文脈をチェックして、妥当な範囲の中で会話が成立しているかを確認するシステムです。\nあなたはユーザーの回答に対して返信しましたが、ユーザーの回答は、その前にあなたが発言した内容に対して適切な回答だったのか、客観的に判断してください。\n出力は以下の通りに行なってください。\n1.回答が適切だった\nture\n2.回答が不適切だった\nfalse" }
+    @check_result = context_check(@prompt)
+    @prompt.pop
+  end
+
+  # ----------再確認用のメッセージを生成----------
+  def message_again
+    # ユーザーにメッセージを再送してもらうためのメッセージ
+    @content = "すみません、よく分かりませんでした。もう一度メッセージを送ってください。"
+    @prompt << { role: "system", content: @content }
+
+    # 性格設定プロンプトの適用
+    m_hash = {message: @content, message_type: 400}
+    @gpt_array << m_hash
+  end
+
   # ----------会話履歴をプロンプトに含む処理----------
   def history
     @messages.each do |message|
@@ -107,8 +146,21 @@ class ChatGptService
       parameters: {
         model: "gpt-3.5-turbo", # Required. # 使用するGPT-3のエンジンを指定
         messages: pronpt,
-        temperature: 0.7, # 応答のランダム性を指定
+        temperature: 1, # 応答のランダム性を指定
         max_tokens: 200,  # 応答の長さを指定
+      },
+    )
+    response['choices'].first['message']['content']
+  end
+
+  # 前後の会話が文脈として適切に繋がっているのかチェック
+  def context_check(pronpt)
+    response = @openai.chat(
+      parameters: {
+        model: "gpt-3.5-turbo", # Required. # 使用するGPT-3のエンジンを指定
+        messages: pronpt,
+        temperature: 0.5, # 応答のランダム性を指定
+        max_tokens: 10,  # 応答の長さを指定
       },
     )
     response['choices'].first['message']['content']
