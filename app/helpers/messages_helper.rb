5585a40addd1cf381558e84ac87146bb4fd611b9
ito-mirai <tetudounohi@gmail.com>
2024-06-12 14:24:29 +0900

    メッセージ画面のビューを設定

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,11 @@
 module MessagesHelper
+  # subjectで指定したmessage_typeがmessagesの中に存在しているか確認するメソッド
+  def check_for_message_existence(messages, subject)
+    judge = messages.select { |message| message.message_type == subject }
+    if judge == []
+      false
+    else
+      true
+    end
+  end
 end
